18e947ef36189994e492a9765f3be3232f94061a
parth9784 <pdsharma458@gmail.com>
2024-07-17 22:15:34 +0530

    initiial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const axios=require("axios")
+const app=express()
+require("dotenv").config()
+// async function getnews(){
+//     return await axios.get("https://newsdata.io/api/1/latest?apikey=pub_487843b54cd00e144ac56f855a5e5e3ba9021&language=en")
+// }
+async function getnews(query) {
+    try {
+        const response = await axios.get(`https://newsdata.io/api/1/latest?apikey=${process.env.API_KEY}&q=${query}&language=en`);
+        return response.data.results;
+    } 
+    catch (error) {
+        console.error("Error fetching news data:", error);
+    }
+}
+app.get("/getnews/:q", async (req,res)=>{
+    let q=req.params.q
+    console.log(q)
+    const newsData = await getnews(q);
+    res.send(newsData)
+})
+app.listen(process.env.PORT,()=>{
+    console.log("Server Started")
+})
